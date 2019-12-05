02e228857ae991d2205c4ca987ebb6345bdcaa4d
Daniel Cregg <daniel.cregg@gmit.ie>
2019-12-05 20:51:58 +0000

    removing dead code

--- updateGitpodConfigs.sh
+++ updateGitpodConfigs.sh
@@ -1,6 +1,5 @@
 wget -q https://raw.githubusercontent.com/danielcregg/ooc2-final-project-template/master/.gitpod.Dockerfile -P /tmp
 wget -q https://raw.githubusercontent.com/danielcregg/ooc2-final-project-template/master/.gitpod.yml -P /tmp
-#if ! [ cmp /tmp/.gitpod.Dockerfile $GITPOD_REPO_ROOT/.gitpod.Dockerfile >/dev/null 2>&1 || cmp /tmp/.gitpod.yml $GITPOD_REPO_ROOT/.gitpod.yml >/dev/null 2>&1 ]; then
 if ! cmp /tmp/.gitpod.Dockerfile $GITPOD_REPO_ROOT/.gitpod.Dockerfile || ! cmp /tmp/.gitpod.yml $GITPOD_REPO_ROOT/.gitpod.yml; then
     mv /tmp/.gitpod.Dockerfile $GITPOD_REPO_ROOT
     mv /tmp/.gitpod.yml $GITPOD_REPO_ROOT
