b9b68cfb51fd9733f9ca2ac21687445b92d376fa
Daniel Cregg <daniel.cregg@gmit.ie>
2019-12-05 14:39:30 +0000

    removed unnecesary imports for buttons

--- src/ie/gmit/mypackage/HelloWorld.java
+++ src/ie/gmit/mypackage/HelloWorld.java
@@ -8,8 +8,6 @@
 package ie.gmit.mypackage;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
