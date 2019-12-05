61f4ddb0826b03ca7edee0cdca4c37f8baaa59fc
Daniel Cregg <daniel.cregg@gmit.ie>
2019-12-05 20:37:30 +0000

    added for loop to check diff in configs

--- updateGitpodConfigs.sh
+++ updateGitpodConfigs.sh
@@ -1,7 +1,12 @@
 wget -q https://raw.githubusercontent.com/danielcregg/ooc2-final-project-template/master/.gitpod.Dockerfile -P /tmp
 wget -q https://raw.githubusercontent.com/danielcregg/ooc2-final-project-template/master/.gitpod.yml -P /tmp
-mv /tmp/.gitpod.Dockerfile $GITPOD_REPO_ROOT
-mv /tmp/.gitpod.yml $GITPOD_REPO_ROOT
-git add .gitpod*
-git commit -m "updated gitpod configs"
-git push
+#if ! [ cmp /tmp/.gitpod.Dockerfile $GITPOD_REPO_ROOT/.gitpod.Dockerfile >/dev/null 2>&1 || cmp /tmp/.gitpod.yml $GITPOD_REPO_ROOT/.gitpod.yml >/dev/null 2>&1 ]; then
+if ! cmp /tmp/.gitpod.Dockerfile $GITPOD_REPO_ROOT/.gitpod.Dockerfile || ! cmp /tmp/.gitpod.yml $GITPOD_REPO_ROOT/.gitpod.yml; then
+    mv /tmp/.gitpod.Dockerfile $GITPOD_REPO_ROOT
+    mv /tmp/.gitpod.yml $GITPOD_REPO_ROOT
+    git add .gitpod
+    git commit -m "updated gitpod configs"
+    git push
+else
+    echo "Your Gitpod configs are up to date."
+fi
