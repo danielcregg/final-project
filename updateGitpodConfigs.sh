76dbb4f89c862a807a3f1c9716095b7380cdfe6f
Daniel Cregg <daniel.cregg@gmit.ie>
2019-12-05 14:46:28 +0000

    hide output from wget

--- updateGitpodConfigs.sh
+++ updateGitpodConfigs.sh
@@ -1,5 +1,5 @@
-wget https://raw.githubusercontent.com/danielcregg/ooc2-final-project-template/master/.gitpod.Dockerfile -P /tmp
-wget https://raw.githubusercontent.com/danielcregg/ooc2-final-project-template/master/.gitpod.yml -P /tmp
+wget https://raw.githubusercontent.com/danielcregg/ooc2-final-project-template/master/.gitpod.Dockerfile -P /tmp 2> /dev/null
+wget https://raw.githubusercontent.com/danielcregg/ooc2-final-project-template/master/.gitpod.yml -P /tmp 2> /dev/null
 mv /tmp/.gitpod.Dockerfile $GITPOD_REPO_ROOT
 mv /tmp/.gitpod.yml $GITPOD_REPO_ROOT
 git add .gitpod*
