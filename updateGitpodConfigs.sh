e1af7d7654d847348795ede992bcfef99fa9bb71
Daniel Cregg <danielcregg@users.noreply.github.com>
2019-12-06 14:48:22 +0000

    added star

--- updateGitpodConfigs.sh
+++ updateGitpodConfigs.sh
@@ -3,7 +3,7 @@ wget -q https://raw.githubusercontent.com/danielcregg/ooc2-final-project-templat
 if ! cmp /tmp/.gitpod.Dockerfile $GITPOD_REPO_ROOT/.gitpod.Dockerfile || ! cmp /tmp/.gitpod.yml $GITPOD_REPO_ROOT/.gitpod.yml; then
     mv /tmp/.gitpod.Dockerfile $GITPOD_REPO_ROOT
     mv /tmp/.gitpod.yml $GITPOD_REPO_ROOT
-    git add .gitpod
+    git add .gitpod*
     git commit -m "updated gitpod configs"
     git push
     echo "Your Gitpod configs have been updated."
